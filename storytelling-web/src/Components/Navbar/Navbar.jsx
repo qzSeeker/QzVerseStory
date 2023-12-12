e262ba38993cf5f23f85e3f6a6c12e0464cddd22
Arpit Yadav <arpityadavay2005@gmail.com>
2023-12-12 21:22:15 +0530

    made nav and side bar

--- storytelling-web/src/Components/Navbar/Navbar.jsx
+++ storytelling-web/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import "./Navbar.scss";
 import { Link } from "react-scroll";
 import "./Navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBarsStaggered,
+  faTimes,
+  faEnvelope,
+  faHome,
+  faPenNib,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Navbar() {
+    const [nav, setNav] = useState(false);
+
+    function handleClick() {
+        setNav(!nav)
+    }
   return (
     <div className="navbar">
       <div className="logo">
@@ -11,11 +26,75 @@ export default function Navbar() {
       </div>
 
       <div className="nav-links">
-        <Link to="hero">Home</Link>
-        <Link to="story">Stories</Link>
-        <Link to="about">About</Link>
-        <Link to="contact">Contact</Link>
+        <nav className="menu">
+              <Link to="hero">
+                <FontAwesomeIcon icon={faHome}/>
+              </Link>
+
+              <Link to="story">
+                <FontAwesomeIcon icon={faPenNib} />
+              </Link>
+
+              <Link to="about">
+                <FontAwesomeIcon icon={faEnvelope}/>
+              </Link>
+
+              <Link to="contact">
+                <FontAwesomeIcon icon={faUser} />
+              </Link>
+
+        </nav>
+
+        {/* Humburger */}
+        <div onClick={handleClick} className="humburger">
+            {!nav ? (
+              <FontAwesomeIcon
+                icon={faBarsStaggered}
+                color="#000"
+                style={{ zIndex: '20', fontSize: '20px', cursor: 'pointer' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faTimes}
+                color="#000"
+                style={{ zIndex: '20', fontSize: '20px', cursor: 'pointer' }}
+              />
+            )}
+          </div>
+
+        {/* Mobile Menu */}
+        <nav className={!nav ? 'hide' : 'mobile-menu'}>
+        <div onClick={handleClick} className="humburger">
+            {!nav ? (
+              <FontAwesomeIcon
+                icon={faBarsStaggered}
+                color="#000"
+                style={{ zIndex: '20', fontSize: '20px', cursor: 'pointer' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faTimes}
+                color="#000"
+                style={{ zIndex: '20', fontSize: '20px', cursor: 'pointer' }}
+              />
+            )}
+          </div>
+          <ul>
+            <li>
+              <Link to="hero">Home</Link>
+            </li>
+            <li>
+              <Link to="story">Stories</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+            <li>
+              <Link to="contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
-  )
+  );
 }
